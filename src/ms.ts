410b686a008666c799a7b3f437f074a45138a176
Divino Borges <divino100rv@gmail.com>
2021-01-24 21:39:27 -0300

    feat: creating the ms function

---
+++ src/ms.ts
@@ -0,0 +1,10 @@
+/**
+ *
+ * @param size - teste
+ * @param ratio - testeee
+ * @param base - testeeeeee
+ * @return tedfd
+ * */
+const ms = (size: number, ratio: number, base: number) => (ratio ** size) * base;
+
+export default ms;
