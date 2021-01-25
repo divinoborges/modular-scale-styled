3a8c67de9da5b25639efa239b3653c6c3e0eea07
Divino Borges <divino100rv@gmail.com>
2021-01-24 21:53:55 -0300

    Updates

---
+++ precache-manifest.52a9b2a1879805b5145e3759bf8b1b93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1658b10f0cfc44ccbcfa22e1a119ca4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ce8c3a1e27718636a2e3",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "887150d0484cfb9ed0cc",
+    "url": "./static/js/2.3464bfd4.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.3464bfd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce8c3a1e27718636a2e3",
+    "url": "./static/js/main.d714b685.chunk.js"
+  },
+  {
+    "revision": "35dbc57b34cce65260cc",
+    "url": "./static/js/runtime-main.3523f3c6.js"
+  }
+]);
